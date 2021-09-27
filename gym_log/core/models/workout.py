4783a64109b183d07f17d4abecf2917f9564a98d
Michael Lenahan <michael_lenahan@hotmail.com>
2021-09-27 13:29:46 +0000

    Add correct datetime usage to workout

--- gym_log/core/models/workout.py
+++ gym_log/core/models/workout.py
@@ -9,8 +9,8 @@ class Workout:
     def __init__(self, name, started_at=None, finished_at=None, tags=[], notes=''):
         self.id = self.generate_id()
         self.name = name
-        self.started_at = datetime.datetime()
-        self.finished_at = datetime.datetime()
+        self.started_at = datetime.time()
+        self.finished_at = datetime.time()
         self.tags = tags
         self.notes = notes
         
