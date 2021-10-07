c426853ece2a05239544f60b723d2ed1415bba3c
Michael Lenahan <michael_lenahan@hotmail.com>
2021-10-07 15:18:08 +0000

    Add logic for testing initializing FileStorage

--- gym_log/core/models/tests/test_filestorage.py
+++ gym_log/core/models/tests/test_filestorage.py
@@ -1,13 +1,12 @@
 import unittest
 
 from gym_log.core.storage.file_storage import FileStorage
-from gym_log.core.models.movement import Movement
 
 
 class TestFileStorage(unittest.TestCase):
 
-    def test_get_entity(self):
-        file_storage = FileStorage('Squat', 'abc', 'abc')
-        movement = Movement('Squat')
-        self.assertEqual(file_storage.get_entity_path, (file_storage.path, movement))
-        
+    file_storage = FileStorage()
+
+    def __init__(self):
+        super(TestFileStorage, self).__init__()
+        FileStorage()
--- gym_log/core/storage/file_storage.py
+++ gym_log/core/storage/file_storage.py
@@ -1,15 +1,10 @@
-from gym_log.core.models.movement import Movement
-from gym_log.core.models.workout import Workout
-from gym_log.core.models.set import Set
-
-
-class FileStorage(Set, Movement, Workout):
+class FileStorage:
 
     path = 'gym_log/core/data/'
-        
+
     def save(self, entity):
         pass
-    
+
     def get_entity_path(self, entity):
         class_name = entity.__class__.__name__
         return self.path + class_name
