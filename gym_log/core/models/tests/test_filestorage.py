2dc3de95417a1448dec9ff3c606f35bb38d909a4
Michael Lenahan <michael_lenahan@hotmail.com>
2021-10-08 10:40:35 +0000

    Add unit tests for testing get_entity_path method from FileStorage class and initialization of other classes

--- gym_log/core/models/tests/test_filestorage.py
+++ gym_log/core/models/tests/test_filestorage.py
@@ -1,12 +1,42 @@
 import unittest
 
 from gym_log.core.storage.file_storage import FileStorage
+from gym_log.core.models.movement import Movement
+from gym_log.core.models.workout import Workout
+from gym_log.core.models.set import Set
 
 
 class TestFileStorage(unittest.TestCase):
 
-    file_storage = FileStorage()
+    def test_initialization_filestorage(self):
+        file_storage = FileStorage()
+        self.assertEqual(file_storage, file_storage)
 
-    def __init__(self):
-        super(TestFileStorage, self).__init__()
-        FileStorage()
+    def test_initialization_movement_name_only(self):
+        movement = Movement('Deadlift')
+        self.assertEqual(movement.name, 'Deadlift')
+
+    def test_initialization_workout_name_only(self):
+        workout = Workout('Leg Day 2')
+        self.assertEqual(workout.name, 'Leg Day 2')
+
+    def test_initialization_set_default_only(self):
+        set = Set(12, workout_id='abc', movement_id='abc')
+        self.assertEqual(set.rep_count, 12)
+        self.assertEqual(set.workout_id, 'abc')
+        self.assertEqual(set.movement_id, 'abc')
+
+    def test_get_entity_path_for_movement(self):
+        movement = Movement('Deadlift')
+        file_storage = FileStorage()
+        self.assertEqual(file_storage.get_entity_path(movement), 'gym_log/core/data/movement')
+
+    def test_get_entity_path_for_workout(self):
+        workout = Workout('Leg Day 1')
+        file_storage = FileStorage()
+        self.assertEqual(file_storage.get_entity_path(workout), 'gym_log/core/data/workout')
+    
+    def test_get_entity_path_for_set(self):
+        set = Set(12, workout_id='abc', movement_id='abc')
+        file_storage = FileStorage()
+        self.assertEqual(file_storage.get_entity_path(set), 'gym_log/core/data/set')
